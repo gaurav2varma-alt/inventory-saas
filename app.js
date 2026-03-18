23be05df33295dba689067cfabdb33bf9a1ab040
gaurav2varma-alt <gaurav2varma@gmail.com>
2026-03-18 16:46:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -190,5 +190,5 @@ app.get("/dashboard/:company_id", async (req, res) => {
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT)
+  console.log("Server running on port " + PORT)
 })
