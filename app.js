88864871059fd8ebb528538e2ae8acbc7759a1a9
gaurav2varma-alt <gaurav2varma@gmail.com>
2026-03-18 17:25:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Home
 app.get("/", (req, res) => {
   res.send("API WORKING")
 })
 
+// Dashboard API
 app.get("/dashboard", (req, res) => {
   res.json({
     salesTotal: 0,
@@ -21,19 +23,7 @@ app.get("/dashboard", (req, res) => {
   })
 })
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log("Server running on " + PORT)
-})  res.json({
-    salesTotal: 0,
-    purchaseTotal: 0,
-    expenseTotal: 0,
-    profit: 0
-  })
-})
-
-// ================= PORT =================
+// PORT
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
